fd6750695a9baec8c43a105019ae3a87cb299acc
fran domovic <fran@calimero.network>
2025-11-28 11:35:33 +0100

    feat: app build

--- app/dev-dist/sw.js
+++ app/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-c6a197bf'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.87346kkc84g"
+    "revision": "0.195spq5c5og"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
